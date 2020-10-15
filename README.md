fffc8e80f96ce88e35492e6fe3821ddd76f913ca
Timon Fiddike <timon-git@fiddike.com>
2020-10-15 17:41:14 +0200

    Move documention from code to README

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # joplin-instant-mail
 Instantly convert email into a note or todo item in Joplin
+
+## prerequisites
+* install Joplin terminal app
+* set up your sync target in the Joplin terminal app, e.g. Dropbox, see Joplin website for instructions
+* install php and composer
+## install script and vendor libraries
+Clone this repository into you users home directory using you favourite git client, then:
+```
+$ cd joplin-instant-mail
+$ composer install
+```
+
+## test that this script cat put notes / todos into joplin and sync:
+Create a notebook called "Inbox" in Joplin. Make sure you set up and sync that notebook or adapt the script to reflect the folder where you want to receive new todos.
+```
+$ cd joplin-instant-mail
+$ cat mail-examples/mail-example-01.txt | php joplin-instant-mail.php
+```
+## connect with your email system / MTA with the script to pass email instantly
+If you are using qmail, this can be achieved as follows:
+For an address of your choice, e.g. joplin-123@..., create a qmail file containing (only) the instruction to pipe any email directly to this script:
+```
+|php $HOME/joplin-instant-mail/joplin-instant-mail.php
+```
+You are done! Send an email to your chosen address, like joplin-123@..., to verify.
--- joplin-instant-mail.php
+++ joplin-instant-mail.php
@@ -1,24 +1,9 @@
 <?php
-// ## prerequisites: installed joplin terminal app, set up your sync target, e.g. dropbox, see joplin website for instructions
-//
-// ## create and sync a joplin notebook called "Inbox", this is where todos from emails are put into.
-//
-// ## install this vendor library https://github.com/zbateson/mail-mime-parser and its dependencies:
-// $ cd joplin-instant-mail
-// $ composer install
-//
-// ## test that this script cat put notes / todos into joplin and sync:
-// $ cd joplin-instant-mail
-// $ cat mail-examples/mail-example-01.txt | php joplin-instant-mail.php
-//
-// ## connect with your email system / MTA to receive email instantly, this example assumes qmail is handling email:
-// ## for an address of your choice, e.g. joplin-123@..., create a qmail file containing (only) the instruction to pipe any email directly to this script:
-// |php $HOME/joplin-instant-mail/joplin-instant-mail.php
-
 require_once __DIR__ . '/vendor/autoload.php';
 
 $message = \ZBateson\MailMimeParser\Message::from(stream_get_contents(STDIN));
-$safeMessageSubject = escapeshellarg($message->getHeaderValue('subject')); 
+
+$safeMessageSubject = escapeshellarg($message->getHeaderValue('subject'));
 $safeMessageText = escapeshellarg($message->getTextContent());
 
 exec('~/.joplin-bin/bin/joplin use Inbox ; \
